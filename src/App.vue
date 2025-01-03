b711c56740ed956ab539681b55b4de2a3c1e7378
yuto0226 <wayne71112@gmail.com>
2025-01-04 01:29:45 +0800

    init: 總之就是出生了
    - vue, vue-router, pinia
    - bootstrap, popperjs
    - fontawesome

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+/* import icons */
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fas, far, fab)
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
