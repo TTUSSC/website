840b395fde23ffbbb04ab4a85c91818d1bf7df4c
yuto0226 <wayne71112@gmail.com>
2025-01-04 18:42:11 +0800

    新增課表表格

--- src/views/LectureView.vue
+++ src/views/LectureView.vue
@@ -2,9 +2,9 @@
 </script>
 
 <template>
-  <div class="row">
-    <div class="col-md-8 mx-auto my-5">
-      <h1>Lecture</h1>
+  <div class="row mx-1 my-5">
+    <div class="col-md-8 mx-auto">
+      <h1 class="mb-4">Lecture</h1>
       <p>
         在科學開源服務社的社課中，我們為社員提供多元且富挑戰性的學習體驗。為了方便大家選擇感興趣的活動，課程難度從🌑🌑🌑🌑🌑到🌕🌕🌕🌕🌕一共分為六個等級：
       </p>
@@ -21,6 +21,56 @@
       <p>
         我們的社課適合各種學習者，內容包括基礎技能教學，如程式語言與軟體使用，實作課程，如網頁開發與機器學習，還有輕鬆類活動如社交與讀書會，讓大家在輕鬆愉快的氛圍中學習與成長。更邀請業界專家分享最新資訊與實戰經驗，拓展視野。無論你是初學者還是資深學習者，這裡都有適合你的課程，帶你一探開源世界的無限可能！
       </p>
+    </div>
+  </div>
+  <div class="row mb-5">
+    <div class="col p-2">
+      <div class="my-2">
+        歷年課表
+      </div>
+      <ul class="nav flex-column nav-pills">
+        <li class="nav-item">
+          <a class="nav-link active">2024 上學期</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">2023 下學期</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">2023 上學期</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">2022 下學期</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">2022 上學期</a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-md-10">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">日期</th>
+            <th scope="col">名稱</th>
+            <th scope="col">難度</th>
+            <th scope="col">講師</th>
+            <th scope="col">地點</th>
+            <th scope="col">標籤</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>9/12</td>
+            <th scope="row">開源茶會</th>
+            <td>🌑🌑🌑🌑🌑</td>
+            <td>威力</td>
+            <td>A3-307</td>
+            <td>
+              <span class="badge text-bg-light">#開源</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
