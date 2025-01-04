eb42931ba7f8eaa35fb79db3a61deef97038d16b
yuto0226 <wayne71112@gmail.com>
2025-01-04 18:47:38 +0800

    feat: 新增專案內容

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -2,5 +2,23 @@
 </script>
 
 <template>
-  <h1>Project</h1>
+  <div class="row my-5">
+    <div class="col-md-8 mx-auto">
+      <h1 class="mb-4">Project</h1>
+
+      <div class="card shadow mb-3">
+        <div class="card-body">
+          <h3 class="card-title mb-2">電資創客營</h3>
+          <p class="card-text">學習基本電學與程式邏輯，帶小朋友完成無用盒小專題。</p>
+        </div>
+      </div>
+
+      <div class="card shadow mb-3">
+        <div class="card-body">
+          <h3 class="card-title mb-2">科學探索家</h3>
+          <p class="card-text">利用簡單的物理知識，帶小朋友動手做出水火箭。</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
