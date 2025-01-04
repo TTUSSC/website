4f7ca82149ecf23ad81bc6913f01681874b95a28
yuto0226 <wayne71112@gmail.com>
2025-01-04 13:49:10 +0800

    feat: 新增路由到 index.js 中並補上對應的頁面檔案

--- src/router/index.js
+++ src/router/index.js
@@ -6,22 +6,27 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      component: () => import('@/views/HomeView.vue'),
     },
     {
       path: '/about',
       name: 'about',
+      component: () => import('@/views/AboutView.vue'),
     },
     {
       path: '/member',
       name: 'member',
+      component: () => import('@/views/MemberView.vue'),
     },
     {
       path: '/lecture',
       name: 'lecture',
+      component: () => import('@/views/LectureView.vue'),
     },
     {
       path: '/project',
       name: 'project',
+      component: () => import('@/views/ProjectView.vue'),
     },
   ],
 })
