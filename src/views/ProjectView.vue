69134c468595c79fa11ee5937a1a52aea6431c32
yuto0226 <wayne71112@gmail.com>
2025-01-19 00:39:53 +0800

    feat: 新增項目&調整樣式

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -3,8 +3,15 @@
 
 <template>
   <div class="row my-5">
-    <div class="col-md-8 mx-auto">
+    <div class="col-md-10 col-lg-8 mx-auto">
       <h1 class="mb-4">Project</h1>
+
+      <div class="card shadow mb-3">
+        <div class="card-body">
+          <h3 class="card-title mb-2">紙上的電腦科學營</h3>
+          <p class="card-text">透過數個學習單活動，帶領小朋友探索電腦科學的知識。</p>
+        </div>
+      </div>
 
       <div class="card shadow mb-3">
         <div class="card-body">
@@ -22,3 +29,16 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.card {
+  border: none !important;
+  transition: .3s transform cubic-bezier(.155, 1.105, .295, 1.12), .3s box-shadow, .3s -webkit-transform cubic-bezier(.155, 1.105, .295, 1.12);
+}
+
+.card:hover {
+  cursor: pointer;
+  transform: scale(1.02);
+  box-shadow: 0 10px 20px rgba(0, 0, 0, .12), 0 4px 8px rgba(0, 0, 0, .06);
+}
+</style>
