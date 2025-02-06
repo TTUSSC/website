b2f0f4639446431599d4a3a41918b839b2956e19
yuto0226 <wayne71112@gmail.com>
2025-02-06 19:02:20 +0800

    feat: 完成歷屆社團成員頁面

--- src/views/MemberView.vue
+++ src/views/MemberView.vue
@@ -1,11 +1,14 @@
 <script setup>
+import { ref } from 'vue';
+
 const baseUrl = import.meta.env.BASE_URL;
 const trueURL = (url) => {
   return `${baseUrl}${url}`
 }
 
 const members = {
-  '113':[
+  // 第拾貳屆
+  '12':[
     {
       image: trueURL("img/member/willy.jpg"),
       name: "游凱崴",
@@ -108,17 +111,173 @@ const members = {
       desc: "電機系學生，能自己完成的就自己做，不忘初心，盡力就好，一切隨緣....",
       tags: []
     },
-  ]
+  ],
+  // 第拾壹屆
+  '11':[
+    {
+      image: trueURL("img/member/yuto.jpg"),
+      name: "羅崧瑋",
+      title: "社長",
+      ig: "sw_1a4",
+      desc: "資工系，熱衷於研究網頁前後端、電腦底層技術，目前正努力學習資訊安全。",
+      tags: [
+        "弓道",
+        "網頁前後端",
+      ]
+    },
+    {
+      image: trueURL("img/member/tty.jpg"),
+      name: "曹莛宥",
+      title: "副社長",
+      ig: "tty_9_28",
+      desc: "電機系",
+      tags: [
+        "社草",
+      ]
+    },
+    {
+      image: trueURL("img/member/stanley.jpg"),
+      name: "陳威翰",
+      title: "副社長",
+      ig: "stanley_1106_",
+      desc: "資工系，喜歡參加各式各樣的活動，努力學習資安中",
+      tags: [
+        "貝斯(新)手",
+        "社花"
+      ]
+    },
+    {
+      image: trueURL("img/member/xin.jpg"),
+      name: "張芯瑜",
+      title: "文書組長",
+      ig: "xin._1865",
+      desc: "電機系",
+      tags: [
+        "不要害SHIU",
+      ]
+    },
+    {
+      image: trueURL("img/member/john.jpg"),
+      name: "林靖疆",
+      title: "財務組長",
+      ig: "jinjohn_lin",
+      desc: "電機系",
+      tags: []
+    },
+    {
+      image: trueURL("img/member/sam.jpg"),
+      name: "陳茂凱",
+      title: "器材組長",
+      ig: "sam_930704",
+      desc: "電機系",
+      tags: []
+    },
+    {
+      image: trueURL("img/member/ju.jpg"),
+      name: "劉玠佑",
+      title: "教學組長",
+      ig: "j.u_28_",
+      desc: "電機系",
+      tags: []
+    },
+    {
+      image: trueURL("img/member/brian.jpg"),
+      name: "Brain",
+      title: "教學組長",
+      ig: "brian_jhan",
+      desc: "資經系",
+      tags: []
+    },
+    {
+      image: trueURL("img/member/harry.jpg"),
+      name: "郭祐亨",
+      title: "美宣組長",
+      ig: "harry.0108_",
+      desc: "工設系",
+      tags: []
+    },
+    {
+      image: trueURL("img/member/xiangen.jpg"),
+      name: "吳享恩",
+      title: "紀錄組長",
+      ig: "xiangen7653",
+      desc: "工設系",
+      tags: []
+    }
+  ],
+  // 第拾屆
+  '10':[
+    {
+      image: trueURL("img/member/timothy.jpg"),
+      name: "Timothy ",
+      title: "社長",
+      ig: "timothy_tzd2819",
+      desc: "電機系",
+      tags: []
+    },
+    {
+      image: trueURL("img/member/yellow_new.jpg"),
+      name: "黃新翰",
+      title: "副社長",
+      ig: "yellow_new_and",
+      desc: "電機系",
+      tags: []
+    },
+    {
+      image: trueURL("img/member/zhen.jpg"),
+      name: "王苡禎 ",
+      title: "文書",
+      ig: "91_12_19",
+      desc: "電機系",
+      tags: []
+    },
+  ],
 };
+
+const years = [
+  { name: '第拾貳屆', value: '12' },
+  { name: '第拾壹屆', value: '11' },
+  { name: '第拾屆', value: '10' },
+];
+
+const selectedYear = ref(years[0]);
+
+function handleSelectYear(year) {
+  selectedYear.value = year;
+}
 </script>
 
 <template>
-  <div class="row my-5">
-    <div class="col-md-10 mx-auto">
+  <div class="row mx-1 my-5">
+    <div class="col-md-8 mx-auto">
       <h1 class="mb-4">社團成員</h1>
+      <p>
+        科學開源服務社的成員來自各個不同的科系，包括電機系、資工系、工設系等。每個人都帶著獨特的專長和興趣，共同為推廣開源文化而努力。我們相信，透過開源精神的交流與分享，能夠創造更多的可能性。
+      </p>
+    </div>
+  </div>
 
+  <div class="row mb-5">
+    <!-- 左側選單 -->
+    <div class="col p-2">
+      <div class="my-2">
+        歷屆成員
+      </div>
+      <ul class="nav flex-column nav-pills">
+        <li class="nav-item mb-1" v-for="year in years" :key="year.value">
+          <a class="nav-link" :class="{ active: selectedYear.value === year.value }" href="#"
+            @click.prevent="handleSelectYear(year)">
+            {{ year.name }}
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <!-- 右側成員卡片 -->
+    <div class="col-lg-10">
+      <h3>{{ selectedYear.name }}</h3>
       <div class="row">
-        <div class="col-sm-12 col-md-6 mb-3" v-for="member in members['113']" :key="member.id">
+        <div class="col-sm-12 col-md-6 mb-3" v-for="member in members[selectedYear.value]" :key="member.name">
           <div class="card h-100">
             <div class="card-body">
               <!-- 大頭貼 + 姓名 + 職稱 + IG -->
@@ -154,6 +313,7 @@ const members = {
     </div>
   </div>
 </template>
+
 <style scoped>
 .card {
   border: none !important;
