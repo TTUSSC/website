307401d801877d725dd1ef6c88c2ee93f3c94e93
yuto0226 <wayne71112@gmail.com>
2025-02-06 19:25:17 +0800

    feat: 新增動態頁面 title

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { ref, watch } from 'vue'
+import { RouterView, useRoute } from 'vue-router'
 
 import NavBar from '@/components/NavBar.vue'
 import PageFooter from './components/PageFooter.vue';
+
+const usePageTitle = () => {
+  const baseTitle = '科學開源服務社';
+  const pageTitle = ref(baseTitle);
+
+  const route = useRoute();
+
+  watch(
+    () => route.meta.title,
+    (newTitle) => {
+      pageTitle.value = newTitle ? `${baseTitle}｜${newTitle}` : baseTitle
+      document.title = pageTitle.value
+    },
+    { immediate: true }
+  );
+
+  return pageTitle;
+}
+
+usePageTitle();
 </script>
 
 <template>
--- src/router/index.js
+++ src/router/index.js
@@ -7,26 +7,41 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('@/views/HomeView.vue'),
+      meta: {
+        title: "首頁"
+      },
     },
     {
       path: '/about',
       name: 'about',
       component: () => import('@/views/AboutView.vue'),
+      meta: {
+        title: "關於"
+      },
     },
     {
       path: '/member',
       name: 'member',
       component: () => import('@/views/MemberView.vue'),
+      meta: {
+        title: "社團成員"
+      },
     },
     {
       path: '/lecture',
       name: 'lecture',
       component: () => import('@/views/LectureView.vue'),
+      meta: {
+        title: "社課"
+      },
     },
     {
       path: '/project',
       name: 'project',
       component: () => import('@/views/ProjectView.vue'),
+      meta: {
+        title: "專案"
+      },
     },
   ],
 })
